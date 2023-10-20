797bca3eefc3fbfb1a6b3a3c2e5e54fa382bf996
RedAngelRedux <sammy@sammynava.com>
2023-10-19 21:49:24 -0700

    Add the Actors Controller.

---
+++ MyFilmBevy/Controllers/ActorsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFilmBevy.Services.Interfaces;
+using NuGet.DependencyResolver;
+
+namespace MyFilmBevy.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _tmdbMappingService;
+
+        public ActorsController(IDataMappingService tmdbMappingService, IRemoteMovieService tmdbMovieService)
+        {
+            _tmdbMappingService = tmdbMappingService;
+            _tmdbMovieService = tmdbMovieService;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.GetActorDetailAsync(id);
+            actor = _tmdbMappingService.MapActorDetail(actor);
+
+            return View(actor);
+        }
+    }
+}
