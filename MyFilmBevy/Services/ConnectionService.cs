8f6affad26a72ab184d7e78af2621101a5eb226c
RedAngelRedux <sammy@sammynava.com>
2023-10-21 09:38:44 -0700

    Fix connectionString for connecting to Heroku-Postgres when running on Heroku

--- MyFilmBevy/Services/ConnectionService.cs
+++ MyFilmBevy/Services/ConnectionService.cs
@@ -6,14 +6,21 @@ namespace MyFilmBevy.Services
     {
         public static string GetConnectionString(IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
             var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+            string? connectionString;
+            connectionString = (string.IsNullOrEmpty(databaseUrl)) 
+                ? configuration.GetConnectionString("DefaultConnection")
+                : BuildConnectionString(databaseUrl);
+            return connectionString;
+            //var connectionString = configuration.GetConnectionString("Heroku-Postgres");
+            //return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
         }
 
         private static string BuildConnectionString(string databaseUrl)
-        {
-            var databaseUri = new Uri("postgres://wxqtzjtswqutka:d03a1168534e58124f8fd021e5112f9124dfefa24bfe8ea8cd9646aea29d4ea6@ec2-54-234-13-16.compute-1.amazonaws.com:5432/dbflefti0ucs4n\r\n");
+        { 
+            //var databaseUri = new Uri("postgres://wxqtzjtswqutka:d03a1168534e58124f8fd021e5112f9124dfefa24bfe8ea8cd9646aea29d4ea6@ec2-54-234-13-16.compute-1.amazonaws.com:5432/dbflefti0ucs4n\r\n");
+            //var databaseUri = new Uri("postgres://wxqtzjtswqutka:d03a1168534e58124f8fd021e5112f9124dfefa24bfe8ea8cd9646aea29d4ea6@ec2-54-234-13-16.compute-1.amazonaws.com:5432/dbflefti0ucs4n");
+            var databaseUri = new Uri(databaseUrl);
             var userInfo = databaseUri.UserInfo.Split(':');
             var builder = new NpgsqlConnectionStringBuilder
             {
